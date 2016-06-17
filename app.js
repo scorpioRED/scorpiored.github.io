666d9393e20e3e9873e3c7d9f34b2a2b1034494f
scorpiored88 <scorpio.red.88@gmail.com>
2016-06-17 15:06:26 +0300

    prommise

--- app.js
+++ app.js
@@ -11,54 +11,29 @@ var pocemonAPP = angular.module('pocemonAPP', [])
             init: function (q) {
                 q = String(q);
 
-
-                //with prommise
-
-                get_request(q,false).then(function(answ){
-                    console.log('then ',answ);
-                    console.log('answer form factory - ', answ['data']['objects'][0]);
+                get_request.get_data(q, false).then(function (answ) {
+                    console.log('ANSWER',answ['data']['objects'][0]);
                     $scope.lists = answ['data']['objects'];
-                    $scope.loading.main = false;
-                    console.log('should hide loading',$scope.loading.main)
-                    $scope.loading.button = false;
-                    angular.forEach($scope.lists, function(single) {
-                        if($scope.pokemon.types.indexOf(single['types'][0]['name']) == -1){
-                            $scope.pokemon.types.push(single['types'][0]['name']);
-                        }else {
-                            if(single['types'][1] && $scope.pokemon.types.indexOf(single['types'][1]['name']) == -1){
-                                $scope.pokemon.types.push(single['types'][1]['name']);
+                        $scope.loading.main = false;
+                        console.log('should hide loading', $scope.loading.main)
+                        $scope.loading.button = false;
+                        angular.forEach($scope.lists, function (single) {
+                            if ($scope.pokemon.types.indexOf(single['types'][0]['name']) == -1) {
+                                $scope.pokemon.types.push(single['types'][0]['name']);
+                            } else {
+                                if (single['types'][1] && $scope.pokemon.types.indexOf(single['types'][1]['name']) == -1) {
+                                    $scope.pokemon.types.push(single['types'][1]['name']);
+                                }
                             }
-                        }
-                    });
+                        });
 
-                    console.log('finn', $scope.pokemon.types)
-
-                })
+                        console.log('finn', $scope.pokemon.types)
 
 
-                //with CB
+                },function (err) {
+                    console.log('factory error  - ', err.data)
+                });
 
-                //get_request(q, false, function (answ) {
-                //    console.log('answer form factory - ', answ['data']['objects'][0]);
-                //    $scope.lists = answ['data']['objects'];
-                //    $scope.loading.main = false;
-                //    $scope.loading.button = false;
-                //    angular.forEach($scope.lists, function (single) {
-                //        if ($scope.pokemon.types.indexOf(single['types'][0]['name']) == -1) {
-                //            $scope.pokemon.types.push(single['types'][0]['name']);
-                //        } else {
-                //            if (single['types'][1] && $scope.pokemon.types.indexOf(single['types'][1]['name']) == -1) {
-                //                $scope.pokemon.types.push(single['types'][1]['name']);
-                //            }
-                //        }
-                //    });
-                //
-                //    console.log($scope.pokemon.types)
-                //
-                //
-                //}, function (err) {
-                //    console.log('factory error  - ', err.data)
-                //})
 
 
             },
@@ -67,16 +42,15 @@ var pocemonAPP = angular.module('pocemonAPP', [])
                 $scope.single_container.spinner = true;
 
                 console.log('id is - ', id);
-                get_request(false, String(id), function (answ) {
+                get_request.get_data(false,String(id)).then( (answ) =>{
                     console.log('answer single - ', answ['data']['name']);
                     $scope.single_details = answ['data'];
                     $scope.single_container.spinner = false;
                     $scope.single_container.all = true;
-
-
-                }, function (err) {
+                },(err) => {
                     console.log('factory error  - ', err.data)
                 })
+
             }
         };
 
@@ -113,40 +87,17 @@ var pocemonAPP = angular.module('pocemonAPP', [])
     })
 
 
-     //with CB
-
-    //.factory('get_request', function ($http) {
-    //    return function (req_url, id, cdsu, cderr) {
-    //        req_url = id ? id : "?limit=" + req_url;
-    //        console.log('url - ', "http://pokeapi.co/api/v1/pokemon/" + req_url);
-    //        $http.get("http://pokeapi.co/api/v1/pokemon/" + req_url).then(cdsu, cderr);
-    //
-    //    }
-    //})
-
 
     .factory('get_request', function ($http) {
-        return function (req_url, id) {
-            return new Promise(function (resolve, reject) {
+        return {
+            get_data: function (req_url, id) {
                 req_url = id ? id : "?limit=" + req_url;
-                console.log('url - ', "http://pokeapi.co/api/v1/pokemon/" + req_url);
-                $http.get("http://pokeapi.co/api/v1/pokemon/" + req_url).then(function (dt) {
-                    if (dt.status == 200) {
-                        resolve(dt)
-                    } else {
-                        var error = new Error(dt.statusText);
-                        error.code = dt.status;
-                        reject(error);
-                    }
-
-                }, function (err) {
-                    if (err) {
-                        reject(new Error("Network Error"));
-                    }
-                });
-            })
-
-
+                return $http.get("http://pokeapi.co/api/v1/pokemon/" + req_url);
+            },
+            otherMethod: function () {
+                return 'other method'
+            }
         }
     })
+
 
